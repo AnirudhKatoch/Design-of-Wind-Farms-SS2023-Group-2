7e77ab14a5e6197c817a9351bd314325f3ddb9c6
AnirudhKatoch <136621943+AnirudhKatoch@users.noreply.github.com>
2023-06-14 17:55:22 +0200

    Add files via upload

---
+++ Wind_rose_Kastrup_DK_120.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.interpolate import NearestNDInterpolator
+from floris.tools import WindRose, FlorisInterface
+
+
+data = pd.read_csv("nc_files/mesotimeseries_Kastrup-DK10m/mesotimeseries_Kastrup-DK10m.csv")
+print("The wind rose dataframe looks as follows: \n\n {} \n".format(data))
+
+#Convert csv columns into arrays
+ws = data['ws'].tolist()
+wd = data['wd'].tolist()
+
+#Adapt the wind speed to the hub-height
+alpha = 0.17
+Hub_Height = 120
+ws = np.multiply(ws,(Hub_Height/10)**alpha)
+
+#Plot the wind rose base on wind speed and wind direction
+wr = WindRose()
+data=wr.make_wind_rose_from_user_data(wd, ws)
+print("The wind rose dataframe looks as follows: \n\n {} \n".format(data))
+
+wr.plot_wind_rose()
+plt.show()
+
+
+# data.to_csv("output_data.csv", index=False)
+# print("CSV file created successfully.")
+
